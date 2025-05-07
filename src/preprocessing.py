e24db181d953dda3ac0459b3f90bc060097b8193
jtrejoo <159100541+jtrejoo@users.noreply.github.com>
2025-05-06 18:13:14 -0700

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,9 @@
-#use this one for cleaning the datasets and prepearing it
+import pandas as pd
+from sklearn.
+
+#assessing and cleaning data, also loading:)
+def load_and_clean_data(filepath):
+    df = pd.read_cvs(filepath).dropna()
+
+#encoding certain variable types
+labeling = {}
